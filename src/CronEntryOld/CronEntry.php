12b56013cf2c7baf2f6af5f824e6561f1fed7b96
kapilsharma <kapil@kapilsharma.info>
2015-04-02 16:45:51 +0530

    Renamed CronEntry to CronEntryOld

--- src/CronEntryOld/CronEntry.php
+++ src/CronEntryOld/CronEntry.php
@@ -24,6 +24,7 @@ class CronEntry {
     private $month = null;
     private $dayOfWeek = null;
     private $command = null;
+    private $cronAdapter = null;
 
     private $user;
     private $file;
